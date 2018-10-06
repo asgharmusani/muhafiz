aec61dc7c46f578f2f0cf788689e99d74663df47
Asghar Musani <asgharmusani@gmail.com>
2018-10-07 00:15:34 +0500

    socket io

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
 var serialport = require("serialport"),
+    SerialPort = serialport.SerialPort, // localize object constructor
+    portName = 'COM3',
     express = require("express"),
     app = express(),
     Handlebars = require("handlebars"),
     exphbs = require("express-handlebars"),
     server = require("http").createServer(app),
+    io = require('socket.io')(server),
     path = require("path"),
 	bodyParser = require("body-parser");
 
@@ -29,18 +32,47 @@ app.use(
 );
 
 
-var port = new serialport("COM7", {
-  baudRate: 9600,
-  parser: new serialport.parsers.Readline("\n")
-});
+var 
 
-port.on("open", function() {
-	console.log('open');
-	port.on('data', function(data) {
-		console.log(data.toString());
-	});
-});
+var connectArd = function() {
+    var sp = new SerialPort(portName, {
+        parser: serialport.parsers.readline("\r\n"),
+        //parser: serialport.parsers.raw,
+        baudrate: 9600,
+        // defaults for Arduino serial communication
+        dataBits: 8,
+        parity: 'none',
+        stopBits: 1,
+        flowControl: false
+    });
 
+
+    sp.on("open", function() {
+        console.log('Serial Port ' + portName + ' Opened');
+        sp.on('data', function(data) {
+            console.log(data.toString());
+            io.emit('arduino', data);
+        });
+    });
+
+    sp.on('close', function(){
+        console.log('ARDUINO PORT CLOSED, waiting 9 sec before retry');
+        setTimeout(
+            function() {
+                //process.exit();
+                reconnectArd();
+            }, 9000);
+    });
+
+    sp.on('error', function (err) {
+        console.error("sp error: ", err, "Waiting 9 sec before restarting");
+        setTimeout(
+            function() {
+                //process.exit();
+                reconnectArd();
+            }, 9000);
+    });
+}
 
 app.use(require("./routes/auth"));
 app.use(require("./routes/dashboard"));
