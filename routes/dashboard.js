b71e3bd29e131c44221bcc197928ac6bd4ebb25c
Asghar Musani <asgharmusani@gmail.com>
2018-10-11 04:01:03 +0500

    commit

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,15 +1,14 @@
 var express = require("express"),
     router = express.Router();
 
-
-router.get("/dashboard", function(req, res, next){
+router.get("/dashboard", function(req, res, next) {
     res.render("dashboard");
 });
 
-router.get("/test", function(req, res, next){
+router.get("/test", function(req, res, next) {
     res.render("test", {
         layout: false,
     });
-})
+});
 
 module.exports = router;
--- routes/socket.js
+++ routes/socket.js
@@ -1,67 +1,59 @@
-
 var SerialPort = require("serialport"),
-    Readline = require('@serialport/parser-readline'),
-    portName = 'COM3';
+    Readline = require("@serialport/parser-readline"),
+    portName = "COM3";
 
 module.exports = function(io) {
-
-    var somedataNSP = io.of('/somedata');
+    var somedataNSP = io.of("/somedata");
     var connectArd = function() {
         const sp = new SerialPort(portName, {
             baudRate: 115200,
             // defaults for Arduino serial communication
             dataBits: 8,
-            parity: 'none',
+            parity: "none",
             stopBits: 1,
-            flowControl: false
-          });
-    
-        const parser = sp.pipe(new Readline({ delimiter: '\r\n' }))
-    
+            flowControl: false,
+        });
+
+        const parser = sp.pipe(new Readline({ delimiter: "\r\n" }));
+
         sp.on("open", function() {
-            console.log('Serial Port ' + portName + ' Opened');
-            parser.on('data', function(data) {
+            console.log("Serial Port " + portName + " Opened");
+            parser.on("data", function(data) {
                 let ardData = data.split(":");
                 let jsonData = {};
-                if(ardData[0] === "GPS"){
+                if (ardData[0] === "GPS") {
                     let gps = ardData[1].split(",");
                     jsonData = {
                         GPS: {
                             lat: gps[0],
                             long: gps[1],
-                        }
-                    }
-                }
-                else
-                    jsonData[ardData[0]] = ardData[1];
-                somedataNSP.emit('arduino', jsonData);
+                        },
+                    };
+                } else jsonData[ardData[0]] = ardData[1];
+                somedataNSP.emit("arduino", jsonData);
             });
         });
-    
-        sp.on('close', function(){
-            console.log('ARDUINO PORT CLOSED, waiting 9 sec before retry');
-            setTimeout(
-                function() {
-                    //process.exit();
-                    reconnectArd();
-                }, 9000);
+
+        sp.on("close", function() {
+            console.log("ARDUINO PORT CLOSED, waiting 9 sec before retry");
+            setTimeout(function() {
+                //process.exit();
+                reconnectArd();
+            }, 9000);
         });
-    
-        sp.on('error', function (err) {
+
+        sp.on("error", function(err) {
             console.error("sp error: ", err, "Waiting 9 sec before restarting");
-            setTimeout(
-                function() {
-                    //process.exit();
-                    reconnectArd();
-                }, 9000);
+            setTimeout(function() {
+                //process.exit();
+                reconnectArd();
+            }, 9000);
         });
-    }
-    
-    
-    
+    };
+
     // check for connection errors or drops and reconnect
-    var reconnectArd = function () {
-        console.log('INITIATING RECONNECT');
+    var reconnectArd = function() {
+        console.log("INITIATING RECONNECT");
         connectArd();
         /*
         setTimeout(function(){
@@ -71,5 +63,4 @@ module.exports = function(io) {
     };
 
     connectArd();
-
-}
+};
