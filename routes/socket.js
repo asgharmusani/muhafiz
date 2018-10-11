3c16a15ea692978f86bb85105269cb19ed961dbd
Samair Zia <samairzia1995@gmail.com>
2018-10-12 00:12:49 +0500

    some changes

--- routes/socket.js
+++ routes/socket.js
@@ -1,76 +1,76 @@
 var SerialPort = require("serialport"),
-    Readline = require("@serialport/parser-readline"),
-    portName = "COM5";
+  Readline = require("@serialport/parser-readline"),
+  portName = "COM4";
 
-module.exports = function(io) {
-    var somedataNSP = io.of("/somedata");
-    var connectArd = function() {
-        const sp = new SerialPort(portName, {
-            baudRate: 115200,
-            // defaults for Arduino serial communication
-            dataBits: 8,
-            parity: "none",
-            stopBits: 1,
-            flowControl: false,
-        });
+module.exports = function (io) {
+  var somedataNSP = io.of("/somedata");
+  var connectArd = function () {
+    const sp = new SerialPort(portName, {
+      baudRate: 115200,
+      // defaults for Arduino serial communication
+      dataBits: 8,
+      parity: "none",
+      stopBits: 1,
+      flowControl: false,
+    });
 
-        const parser = sp.pipe(new Readline({ delimiter: "\r\n" }));
+    const parser = sp.pipe(new Readline({ delimiter: "\r\n" }));
 
-        sp.on("open", function() {
-            console.log("Serial Port " + portName + " Opened");
-            parser.on("data", function(data) {
-                let ardData = data.split("/");
-                if(typeof ardData[1] != "undefined"){
-                    let idAndVal = ardData[1].split(":");
-                    // console.log(ardData);
-                    let jsonData = {};
-                    if (ardData[0] === "GPS") {
-                        let gps = idAndVal[1].split(",");
-                        jsonData = {
-                            id: idAndVal[0],
-                            gps: {
-                                lat: gps[0],
-                                long: gps[1],
-                            },
-                        };
-                    } else {
-                        jsonData["id"] = idAndVal[0];
-                        jsonData[ardData[0]] = idAndVal[1];
-                    }
-                    console.log(jsonData);
-                    somedataNSP.emit("arduino", jsonData);
-                }
-                
-            });
-        });
+    sp.on("open", function () {
+      console.log("Serial Port " + portName + " Opened");
+      parser.on("data", function (data) {
+        let ardData = data.split("/");
+        if (typeof ardData[1] != "undefined") {
+          let idAndVal = ardData[1].split(":");
+          // console.log(ardData);
+          let jsonData = {};
+          if (ardData[0] === "GPS") {
+            let gps = idAndVal[1].split(",");
+            jsonData = {
+              id: idAndVal[0],
+              gps: {
+                lat: gps[0],
+                long: gps[1],
+              },
+            };
+          } else {
+            jsonData["id"] = idAndVal[0];
+            jsonData[ardData[0]] = idAndVal[1];
+          }
+          console.log(jsonData);
+          somedataNSP.emit("arduino", jsonData);
+        }
 
-        sp.on("close", function() {
-            console.log("ARDUINO PORT CLOSED, waiting 9 sec before retry");
-            setTimeout(function() {
-                //process.exit();
-                reconnectArd();
-            }, 9000);
-        });
+      });
+    });
 
-        sp.on("error", function(err) {
-            console.error("sp error: ", err, "Waiting 9 sec before restarting");
-            setTimeout(function() {
-                //process.exit();
-                reconnectArd();
-            }, 9000);
-        });
-    };
+    sp.on("close", function () {
+      console.log("ARDUINO PORT CLOSED, waiting 9 sec before retry");
+      setTimeout(function () {
+        //process.exit();
+        reconnectArd();
+      }, 9000);
+    });
 
-    // check for connection errors or drops and reconnect
-    var reconnectArd = function() {
-        console.log("INITIATING RECONNECT");
-        connectArd();
-        /*
-        setTimeout(function(){
-            console.log('RECONNECTING TO ARDUINO [after 5sec wait]');
-            connectArd();
-        }, 5000);*/
-    };
+    sp.on("error", function (err) {
+      console.error("sp error: ", err, "Waiting 9 sec before restarting");
+      setTimeout(function () {
+        //process.exit();
+        reconnectArd();
+      }, 9000);
+    });
+  };
 
+  // check for connection errors or drops and reconnect
+  var reconnectArd = function () {
+    console.log("INITIATING RECONNECT");
     connectArd();
+    /*
+    setTimeout(function(){
+        console.log('RECONNECTING TO ARDUINO [after 5sec wait]');
+        connectArd();
+    }, 5000);*/
+  };
+
+  connectArd();
 };
